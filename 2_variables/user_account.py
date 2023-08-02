edd58278593561c6968489b28318a6dbc491d1aa
clara <10088152+claraj@users.noreply.github.com>
2023-08-02 17:44:50 -0500

    More assignment

--- 2_variables/user_account.py
+++ 2_variables/user_account.py
@@ -1,19 +1,19 @@
-
 post_likes = 28
 
-# if we like this post
-post_likes = post_likes + 1
-# todo - display the new number of likes
+# user likes this post - need to update the value of the variable
+post_likes = post_likes + 1  # modifying the value of post_likes with assignment
 
-# if we un-like this post
-post_likes = post_likes - 1
-# todo - display the new number of likes
+print(post_likes)
 
+# User un-likes the post
+post_likes = post_likes - 1  # modifying the value of post_likes with assignment
 
-# Create a message to display the number of likes
-# Instagram says "28 likes"
+# Create a message to display with the number of likes
 message = str(post_likes) + ' likes'
 
+print(message)
+
+# Create the page title from the username variable
 
 username = 'google'
 
@@ -22,6 +22,9 @@ page_title = '@' + username
 print(page_title)
 
 
+# Switch to a different username
+
+# Work with the number of followers
 
 username = 'minneapolis_college'
 
@@ -32,5 +35,12 @@ followers = 1522
 followers = followers + 1
 
 # Optional - printing with commas
-
 print(f'{followers:,d}')   # prints 1,522
+
+## Checking the length of post text
+
+post_text = 'I met a llama this summer!'
+
+post_text_length = len(post_text)
+
+print(post_text_length)
